4e9082dcd6f11994145fa53d0adcfc6a2e2c78f1
Charles Smith <charles@sollum.tech>
2021-07-25 16:06:05 -0400

    use overridable methods for connect/disconnect events

--- src/webmesh/websocket_server.py
+++ src/webmesh/websocket_server.py
@@ -1,7 +1,9 @@
 import asyncio
 import functools
 import json
+import logging
 import signal
+import platform
 import uuid
 from multiprocessing.pool import ThreadPool
 
@@ -9,14 +11,23 @@ import websockets
 from websockets.exceptions import WebSocketException
 
 
+logging.basicConfig(level=logging.DEBUG)
+
+
 class WSServer:
-    def __init__(self, host: str = '0.0.0.0', port: int = 8080):
+    def __init__(self, host: str = '0.0.0.0', port: int = 8080, debug: bool = False):
         self.host = host
         self.port = port
         self.server = None
         self.consumers = {}
         self.clients = {}
         self.thread_pool = ThreadPool()
+        self.logger = logging.getLogger('webmesh.server')
+
+        if not debug:
+            logging.getLogger('websockets.server').disabled = True
+            logging.getLogger('websockets.protocol').disabled = True
+            logging.getLogger('asyncio').disabled = True
 
     def on(self, path):
         def wrapper(func):
@@ -31,20 +42,31 @@ class WSServer:
     def on_not_found(self, payload, path, id):
         return json.dumps('Path not found')
 
-    def on_connect(self, websocket):
+    def _on_connect(self, websocket):
         id = uuid.uuid4().hex
-        self.clients[id] = websocket
-        print(f'[{id}] Client connected !')
+        self.clients[id] = {'socket': websocket}
+        self.on_connect(id, websocket)
         return id
 
+    def on_connect(self, id: str, websocket):
+        self.logger.info(f'Client \'{id}\' connected.')
+
+    def _on_disconnect(self, id: str, websocket):
+        del self.clients[id]
+        self.on_disconnect(id, websocket)
+        return id
+
+    def on_disconnect(self, id: str, websocket):
+        self.logger.info(f'Client \'{id}\' disconnected.')
+
     async def handler(self, websocket, path):
-        id = self.on_connect(websocket)
+        id = self._on_connect(websocket)
         try:
             async for message in websocket:
                 message = json.loads(message)
                 path = message['path']
                 data = message['data'] if 'data' in message else None
-                print(f'[{id}] Message received on {path}: {data}')
+                self.logger.debug(f'[{id}] Message received on {path}: {data}')
                 if path in self.consumers:
                     consumer = self.consumers[path]
                     response = self.thread_pool.apply(consumer, args=[data, path, id])
@@ -55,8 +77,7 @@ class WSServer:
         except WebSocketException:
             pass
         finally:
-            del self.clients[id]
-            print(f'[{id}] Client disconnected !')
+            self._on_disconnect(id, websocket)
 
     async def run(self, stop):
         self.server = websockets.serve(self.handler, self.host, self.port)
@@ -70,8 +91,9 @@ class WSServer:
             loop = asyncio.new_event_loop()
 
         stop = loop.create_future()
-        loop.add_signal_handler(signal.SIGINT, stop.set_result, None)
-        loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+        if platform.system == 'Linux':
+            loop.add_signal_handler(signal.SIGINT, stop.set_result, None)
+            loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
 
         loop.run_until_complete(self.run(stop))
 
