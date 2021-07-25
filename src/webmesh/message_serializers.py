812a468ca5a0113863c2c8c18cd797d09c4c6002
Charles Smith <charles@sollum.tech>
2021-07-25 19:36:22 -0400

    added compression to the webpack serializer

--- src/webmesh/message_serializers.py
+++ src/webmesh/message_serializers.py
@@ -1,6 +1,7 @@
 import json
 from abc import ABC, abstractmethod
 from typing import Any
+import zlib
 
 import msgpack
 
@@ -24,10 +25,17 @@ class StandardJsonSerializer(AbstractMessageSerializer):
 
 
 class MessagePackSerializer(AbstractMessageSerializer):
+    def __init__(self, compressor=zlib):
+        self.compressor = compressor
+
     def serialize(self, obj: Any) -> str:
         msg_bytes = msgpack.dumps(obj)
+        if self.compressor is not None:
+            msg_bytes = self.compressor.compress(msg_bytes)
         return msg_bytes.hex()
 
     def deserialize(self, data: str) -> Any:
         bytes_data = bytes.fromhex(data)
+        if self.compressor is not None:
+            bytes_data = self.compressor.decompress(bytes_data)
         return msgpack.loads(bytes_data)
